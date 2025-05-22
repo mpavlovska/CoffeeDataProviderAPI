1295b8c40cc38499a95261a51ec1efbb07c5db97
Marija Pavlovska <marijapavlovska21@gmail.com>
2025-05-22 21:31:15 +0200

    initial commit

---
+++ Controllers/CoffeeTypesController.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeDataProviderAPI.Data;
+using CoffeeDataProviderAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeDataProviderAPI.Controllers;
+
+[ApiController]
+[Route("api/external-coffee-types")]
+public class CoffeeTypesController : ControllerBase
+{
+    private readonly CoffeeDataService _data;
+
+    public CoffeeTypesController(CoffeeDataService data)
+    {
+        _data = data;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<CoffeeType>> Get()
+    {
+        return Ok(_data.GetAll());
+    }
+}
---
+++ Data/CoffeeDataService.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeDataProviderAPI.Models;
+using System.Text.Json;
+
+namespace CoffeeDataProviderAPI.Data;
+
+public class CoffeeDataService
+{
+    private readonly IWebHostEnvironment _env;
+    private List<CoffeeType>? _coffeeTypes;
+
+    public CoffeeDataService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public IEnumerable<CoffeeType> GetAll()
+    {
+        if (_coffeeTypes is not null) return _coffeeTypes;
+
+        var path = Path.Combine(_env.ContentRootPath, "coffee-types.json");
+
+        if (!File.Exists(path))
+            return Enumerable.Empty<CoffeeType>();
+
+        var json = File.ReadAllText(path);
+        _coffeeTypes = JsonSerializer.Deserialize<List<CoffeeType>>(json);
+
+        return _coffeeTypes ?? Enumerable.Empty<CoffeeType>();
+    }
+}
